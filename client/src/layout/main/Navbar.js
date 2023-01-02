640f95e302923d58a7743919394f89e1c5801620
hasibulislam999 <hasib143sl@gmail.com>
2023-01-02 14:42:21 +0600

    navbar modify after word user finder

--- client/src/layout/main/Navbar.js
+++ client/src/layout/main/Navbar.js
@@ -7,7 +7,9 @@ import auth from "../../config/firebase.config";
 
 const Navbar = () => {
   const { pathname } = useLocation();
-  const { email, role } = useSelector((state) => state.auth);
+  const {
+    user: { email, role },
+  } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   return (
