49f0452473cc1fc8e8767e75ba0290711ecf54f6
hasibulislam999 <hasib143sl@gmail.com>
2023-01-02 14:43:43 +0600

    jobs page init

--- client/src/pages/Jobs.js
+++ client/src/pages/Jobs.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useGetJobsQuery } from "../features/job/jobApi";
 
 const Jobs = () => {
   const navigate = useNavigate();
+  const { data } = useGetJobsQuery();
 
   return (
-    <div className='pt-14'>
+    <div className="pt-14">
       <h1>This is job page</h1>
       <div>
-        <h1>THIS IS A JOB</h1>
-        <button className='border' onClick={() => navigate("/job-details")}>
-          Details
-        </button>
+        {data?.data?.map(({ _id, companyName, position }) => (
+          <div key={_id}>
+            <h2>{companyName}</h2>
+            <h2>{position}</h2>
+            <button onClick={() => navigate(`/job-details/${_id}`)}>
+              Job Details
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
