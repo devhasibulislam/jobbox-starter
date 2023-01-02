3e570bee5417d2e674cb728b5bf4995885b27b9a
hasibulislam999 <hasib143sl@gmail.com>
2023-01-02 14:43:33 +0600

    job details page init

--- client/src/pages/JobDetails.js
+++ client/src/pages/JobDetails.js
@@ -1,14 +1,16 @@
 import React from "react";
-
-import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useGetJobQuery } from "../features/job/jobApi";
 
 const JobDetails = () => {
-  const navigate = useNavigate();
+  const { id } = useParams();
+  const { data } = useGetJobQuery(id);
+  const { position } = data?.data || {};
 
   return (
-    <div className='pt-14'>
+    <div className="pt-14">
       <h1>this is job details</h1>
-      <button className='border'>Apply</button>
+      <button className="border">{position}</button>
     </div>
   );
 };
