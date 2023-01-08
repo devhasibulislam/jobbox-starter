1ca92d0ff55523d188e9a91ec6e52f713d8cc0ce
hasibulislam999 <hasib143sl@gmail.com>
2023-01-08 21:35:43 +0600

    new routes integrate

--- client/src/layout/dashboard/Sidebar.js
+++ client/src/layout/dashboard/Sidebar.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
 const Sidebar = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  const employerRoutes = [
+    {
+      path: "add-job",
+      name: "Add Job",
+    },
+  ];
+  const candidateRoutes = [
+    {
+      path: "applied-jobs",
+      name: "Applied Jobs",
+    },
+  ];
+
   return (
     <div className="bg-primary/10 col-span-2 h-screen sticky top-0">
       <ul className="flex flex-col gap-2 w-full h-full  p-3">
@@ -12,14 +29,28 @@ const Sidebar = () => {
           </Link>
           <h1 className="text-xl">Dashboard</h1>
         </div>
-        <li>
-          <Link
-            className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
-            to="add-job"
-          >
-            Add Job
-          </Link>
-        </li>
+        {user?.role === "employer" &&
+          employerRoutes.map(({ path, name }) => (
+            <li key={name}>
+              <Link
+                className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
+                to={path}
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
+        {user?.role === "candidate" &&
+          candidateRoutes.map(({ path, name }) => (
+            <li key={name}>
+              <Link
+                className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
+                to={path}
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
