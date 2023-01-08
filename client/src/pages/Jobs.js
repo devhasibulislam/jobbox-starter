27a81fb4df8b8b4734a1b476175e4a10932879c9
hasibulislam999 <hasib143sl@gmail.com>
2023-01-08 21:36:05 +0600

    jobs route modify

--- client/src/pages/Jobs.js
+++ client/src/pages/Jobs.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import JobCard from "../components/reusable/JobCard";
 import { useGetJobsQuery } from "../features/job/jobApi";
+import Loading from "../components/reusable/Loading";
 
 const Jobs = () => {
-  const navigate = useNavigate();
-  const { data } = useGetJobsQuery();
+  const { data, isLoading } = useGetJobsQuery();
+  const jobs = data?.data || {};
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <div className="pt-14">
-      <h1>This is job page</h1>
-      <div>
-        {data?.data?.map(({ _id, companyName, position }) => (
-          <div key={_id}>
-            <h2>{companyName}</h2>
-            <h2>{position}</h2>
-            <button onClick={() => navigate(`/job-details/${_id}`)}>
-              Job Details
-            </button>
-          </div>
+      <div className="bg-primary/10 p-5 rounded-2xl">
+        <h1 className="font-semibold text-xl">Find Jobs</h1>
+      </div>
+      <div className="grid grid-cols-3 gap-5 mt-5">
+        {jobs?.map((job) => (
+          <JobCard key={job?._id} jobData={job} />
         ))}
       </div>
     </div>
