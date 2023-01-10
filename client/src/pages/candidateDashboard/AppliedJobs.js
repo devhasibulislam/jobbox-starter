6ecb4bf61edf3ca2cc110c5e0825eef2f8e9a668
hasibulislam999 <hasib143sl@gmail.com>
2023-01-10 22:11:18 +0600

    applied jobs card modify

--- client/src/pages/candidateDashboard/AppliedJobs.js
+++ client/src/pages/candidateDashboard/AppliedJobs.js
@@ -16,11 +16,11 @@ const AppliedJobs = () => {
 
   return (
     <div>
-      <h1 className="text-xl py-5">Applied jobs</h1>
+      <h1 className="text-xl py-5">Applied jobs: <span className="font-semibold">{data?.data?.length}</span></h1>
       <div className="grid grid-cols-2 gap-5 pb-5">
         {data?.data?.map((job) => (
           <JobCard jobData={job} />
-        ))}
+        )).reverse()}
       </div>
     </div>
   );
