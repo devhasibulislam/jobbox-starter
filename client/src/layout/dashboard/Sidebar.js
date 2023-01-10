9d21e150a490c6a8449dfe4f1717f0b33db95ddb
hasibulislam999 <hasib143sl@gmail.com>
2023-01-10 22:11:07 +0600

    integrate new side route

--- client/src/layout/dashboard/Sidebar.js
+++ client/src/layout/dashboard/Sidebar.js
@@ -11,6 +11,10 @@ const Sidebar = () => {
       path: "add-job",
       name: "Add Job",
     },
+    {
+      path: "job-list",
+      name: "Job List",
+    },
   ];
   const candidateRoutes = [
     {
